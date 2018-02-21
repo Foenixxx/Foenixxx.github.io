cafbb0efd95e2e3c222226abace968e3d70a708e
Foenixxx <36118086+Foenixxx@users.noreply.github.com>
2018-02-22 02:21:44 +0300

    Add files via upload

---
+++ 88.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+echo "Hello World"
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential
+sudo apt-get install libssl-dev
+sudo apt-get install libboost-all-dev
+sudo apt-get install libqrencode-dev
+sudo apt-get install libgmp3-dev
+sudo apt-get install miniupnpc
+sudo apt-get install libminiupnpc-dev
+sudo apt-get install libcurl4-openssl-dev
+sudo apt-get install dh-autoreconf
+sudo apt-get install autoconf
+sudo apt-get install automake
+sudo apt-get install git nano
+sudo apt-get install pkg-config
+sudo apt-get install libtool
+sudo apt-get install libgmp-dev
+sudo apt-get install libevent-dev
+
